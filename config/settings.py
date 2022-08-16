861590db20b41ee1103a7ce7262f9de5b5a1a02d
@muhammadibragimov439 <muhammadibragimov439@gmail.com>
2022-08-16 18:44:40 +0500

    UPDATE

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = env.str('SECRET_KEY', 'foo')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', 0)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
 # Application definition
 
